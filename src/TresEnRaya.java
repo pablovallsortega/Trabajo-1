b77a4ea6d131cd92a2bde76cb148c170c6f5c81f
Pablo Valls Ortega <pabloalmerimar@gmail.com>
2024-01-31 09:48:31 +0100

    segundo commit ( 2 cuadro)

--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class TresEnRaya {
 
@@ -8,7 +9,36 @@ public class TresEnRaya {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int opcion = 0;
+        do {
+            System.out.println("MENÚ");
+            System.out.println("1.Jugar");
+            System.out.println("2. Ver estadísticas");
+            System.out.println("0. Salir");
+            try {
+                System.out.println("seleccionar opcion (1-2) o 0 para salir:");
+                opcion = input.nextInt();
+            } catch (InputMismatchException ime) {
+                opcion = 100;
 
+            }
+            input.nextLine();
+            switch (opcion) {
+                case 1:
+                    jugar(input);
+                    break;
 
+                case 2:
+                    System.out.println("Victorias: " + victorias);
+                    System.out.println("Derrotas:" + derrotas + "\n");
+                    break;
+                case 0:
+                    System.out.println("Fin de programa \n");
+                    break;
+                default:
+                    System.out.println("Opción no válida \n");
+                    break;
+            }
+        }while (opcion!=0);
+        input.close();;
     }
 }
